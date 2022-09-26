797aebb4f828143edd07b3d9d981a17359758468
Av0ff <vingaardium@gmail.com>
2022-09-26 18:02:19 +0300

    Add: Collectable Items With Effects
    When player picked up item the effect is applied

---
+++ Scripts/Brain.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Brain : MonoBehaviour
+{
+    [SerializeField]
+    private Health _playerHealth;
+
+    [SerializeField]
+    private Collectable _collectableItem = null;
+
+    private void Update()
+    {
+        FindItem();
+    }
+
+    private void FindItem()
+    {
+        if (_collectableItem != null)
+        {
+            var hpRate = _collectableItem.AttachedItem;
+            _playerHealth.AddEffect(hpRate.HpRange, hpRate.Duration);
+            Destroy(_collectableItem.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Collectable>())
+        {
+            _collectableItem = collision.GetComponent<Collectable>();
+        }
+    }
+}
---
+++ Scripts/CollectableItems.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(menuName = "CollectableItems", order = 0)]
+public class CollectableItems : ScriptableObject
+{
+    public string Name;
+
+    public float HpRange;
+
+    public float Duration;
+}
